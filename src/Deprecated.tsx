7df3a658469b2f97aa0046af712ee8131fd8d64f
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2020-01-23 10:31:18 -0400

    refactor: improve typing support and docs

--- src/Deprecated.tsx
+++ src/Deprecated.tsx
@@ -1,6 +1,6 @@
 import { ComponentType } from "react";
 
-export function deprecatedComponent(reason: string, DeprecatedComponent: ComponentType<any>) {
+export function deprecatedComponent<Props>(reason: string, DeprecatedComponent: ComponentType<Props>) {
     const message = `${DeprecatedComponent.name || 'Anonymous function' } was deprecated: ${reason}`
     console.warn(message)
     return DeprecatedComponent
--- test/Deprecated.test.tsx
+++ test/Deprecated.test.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {memo} from "react";
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { deprecatedComponent } from "../src/Deprecated";
 
 const SampleConstComponent = () => {
+  return null
+}
+
+const SampleConstComponentWithProps = ({}:{attribute: string, method(parameter: string): void}) => {
   return null
 }
 
@@ -25,7 +29,7 @@ describe('Deprecated', () => {
     consoleWarnSpy.mockClear()
   })
 
-  it('shows the warning on console', () => {
+  it('shows the warning on console with function component', () => {
     const DeprecatedComponent =  deprecatedComponent(reason, SampleFunctionComponent)
 
     render(<DeprecatedComponent />)
@@ -33,7 +37,7 @@ describe('Deprecated', () => {
     expect(consoleWarnSpy).toHaveBeenCalledWith(`${SampleFunctionComponent.name} was deprecated: ${reason}`)
   });
 
-  it('shows the warning on console', () => {
+  it('shows the warning on console with const component', () => {
     const DeprecatedComponent =  deprecatedComponent(reason, SampleConstComponent)
 
     render(<DeprecatedComponent />)
@@ -41,7 +45,7 @@ describe('Deprecated', () => {
     expect(consoleWarnSpy).toHaveBeenCalledWith(`${SampleConstComponent.name} was deprecated: ${reason}`)
   });
 
-  it('shows the warning on console', () => {
+  it('shows the warning on console with class component', () => {
     const DeprecatedComponent =  deprecatedComponent(reason, SampleClassComponent)
 
     render(<DeprecatedComponent />)
@@ -49,11 +53,27 @@ describe('Deprecated', () => {
     expect(consoleWarnSpy).toHaveBeenCalledWith(`${SampleClassComponent.name} was deprecated: ${reason}`)
   });
 
-  it('shows the warning on console', () => {
+  it('shows the warning on console with anonymous function component', () => {
     const DeprecatedComponent =  deprecatedComponent(reason, () => null)
 
     render(<DeprecatedComponent />)
 
     expect(consoleWarnSpy).toHaveBeenCalledWith(`Anonymous function was deprecated: ${reason}`)
+  });
+
+  it('shows the warning on console with memoized component', () => {
+    const DeprecatedComponent =  deprecatedComponent(reason, memo(() => null))
+
+    render(<DeprecatedComponent />)
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith(`Anonymous function was deprecated: ${reason}`)
+  });
+
+  it('shows the warning on console with memoized component', () => {
+    const DeprecatedComponent =  deprecatedComponent(reason, SampleConstComponentWithProps)
+
+    render(<DeprecatedComponent attribute="" method={_ => {}}  />)
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith(`SampleConstComponentWithProps was deprecated: ${reason}`)
   });
 });
